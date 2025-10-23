cc4dd285a55bd7d4d3819675502299ee55da7007
CloudtechShankar <shankaryalagandula2004@gmail.com>
2025-10-23 12:21:31 +0530

    Update initial result value in factorial function
    Changed initial result value from 1 to 2 in factorial calculation.

--- fapp.py
+++ fapp.py
@@ -7,7 +7,7 @@ def factorial(n):
     elif n == 0 or n == 1:
         return 1
     else:
-        result = 1
+        result = 2
         for i in range(2, n + 1):
             result *= i
         return result
